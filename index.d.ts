0e57250b3bcc7003f8e95cc09de6c12ff9941bfd
ERU <enteocode@gmail.com>
2018-06-03 01:30:41 +0200

    Added type-definitions

---
+++ index.d.ts
@@ -0,0 +1,27 @@
+declare module "ttfautohint"
+{
+    import { Transform } from "stream";
+
+    export = TTFAutohint;
+
+    interface TTFAutohintOptions
+    {
+        extended: boolean,
+        icon: boolean,
+        info: boolean,
+        hintingLimit: number,
+        min: number,
+        max: number,
+        reference: string,
+        size: number
+    }
+
+    class TTFAutohint extends Transform
+    {
+        options: TTFAutohintOptions;
+
+        static compile(sourceFile: string, targetFile: string, options?: TTFAutohintOptions): TTFAutohint;
+
+        constructor(options?: TTFAutohintOptions);
+    }
+}
